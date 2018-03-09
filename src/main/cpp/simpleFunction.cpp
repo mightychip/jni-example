d6186b89e05beda4184dfdaa9fd1209e44195000
mightychip <mighty.chip@gmail.com>
2018-03-09 09:11:36 -0500

    Oh, hey, you'll need these parts.  I also cleaned up the CMakeLists.txt

---
+++ src/main/cpp/simpleFunction.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Mighty Chip on 2018-03-09.
+//
+
+#include "simpleFunction.h"
+
+const char* sayHello() {
+    return "Hello!";
+}
---
+++ src/main/cpp/simpleFunction.h
@@ -0,0 +1,14 @@
+//
+// Created by Mighty Chip on 2018-03-09.
+//
+
+#ifndef CPP_SIMPLEFUNCTION_H
+#define CPP_SIMPLEFUNCTION_H
+
+
+#include <string>
+
+const char* sayHello();
+
+
+#endif //CPP_SIMPLEFUNCTION_H
